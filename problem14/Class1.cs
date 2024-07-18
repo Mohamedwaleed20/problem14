a54efec7a9e9a4ee5ede0d63aad1e650b937fcc3
Mohamed waleed <Mohamed waleed@DESKTOP-0AUSUD8>
2024-07-18 20:23:13 +0300

    Add project files.

---
+++ problem14/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem14
+{
+    internal class Class1
+    {
+        public class calculator<T> where T : struct, IComparable, IFormattable, IConvertible, IComparable<T>, IEquatable<T>
+        {
+            public delegate T AddDelegate(T a, T b);
+            public delegate T SubDelegate(T a, T b);
+            public delegate T MulDelegate(T a, T b);
+            public delegate T DivDelegate(T a, T b );
+            public T addDelegate(T a, T b,AddDelegate total )
+            {
+                return total(a,b);
+            }
+            public T subtractDelegate(T a,T b,SubDelegate total)
+            {
+               return total(a,b);
+            }
+            public T multiplyDelegate(T a, T b, MulDelegate total)
+            {
+                return total(a, b);
+            }
+            public T divideDelegate(T a, T b, DivDelegate total)
+            {
+                return total(a, b);
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ problem14/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+using static problem14.Class1;
+
+namespace problem14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            calculator<int> intCalculator = new calculator<int>();
+            calculator<int>.AddDelegate addDelegate = (a, b) => a + b;
+            calculator<int>.SubDelegate subDelegate = (a,b) => a - b;
+            calculator<int>.MulDelegate mulDelegate = (a,b) => a * b;
+            calculator<int>.DivDelegate divDelegate = (a,b) => a/ b;
+            int sum = intCalculator.addDelegate(10, 5, addDelegate);
+            int difference = intCalculator.subtractDelegate(10, 5, subDelegate);
+            int product = intCalculator.multiplyDelegate(10, 5, mulDelegate);
+            int quotient = intCalculator.divideDelegate(10, 5, divDelegate);
+            Console.WriteLine($"""
+                Sum: {sum}
+                Difference: {difference}
+                Product: {product}
+                Quotient: {quotient}
+                """);
+
+            Console.ReadLine();
+        }
+    }
+}
